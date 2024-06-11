60da3ce52c2625cafd19de2c7398d31af67a7c1d
martomz <tomataomarjohn@gmail.com>
2024-06-11 22:57:26 +0800

    add image in home screen and initial texts

--- frontend/src/components/navbar/MobileNav.jsx
+++ frontend/src/components/navbar/MobileNav.jsx
@@ -1,11 +1,20 @@
+import { useState, useEffect } from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import NavToggle from '../script_dependencies/NavToggle';
 import navLogo from '../../images/MT.svg';
 
 const MobileNav = () => {
+
+    const [show, setShow] = useState('flex')
+
+    useEffect(() => {
+        const home = document.querySelector('.home');
+        if (home) setShow('none'); else setShow('flex')
+    }, [])
+    
     return ( 
-        <Navbar id="navbar" className="navbar d-md-none fixed-top">
+        <Navbar id="navbar" className="navbar d-md-none fixed-top" style={{display: show}}>
             {/* Collapse button */}
             <button className="navbar-toggler hamburger-button" type="button" onClick={NavToggle} style={{ zIndex: 10 }}>
                 <div className="animated-icon">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,53 @@
+import { format, getHours } from 'date-fns';
 import HideSideNav from "../components/script_dependencies/HideSideNav";
+import avatar from '../images/martoms-transparent.svg'
 
 const Home = () => {
+
+    // Get day and time greetings
+    const getCurrentDayAndTimeOfDay = () => {
+        const now = new Date();
+      
+        // Get the current day
+        const day = format(now, 'EEEE'); 
+      
+        // Get the current hour
+        const hour = getHours(now);
+      
+        // Determine the time of day
+        let timeOfDay;
+        if (hour < 12) {
+          timeOfDay = 'morning';
+        } else if (hour === 12) {
+          timeOfDay = 'noon';
+        } else if (hour > 12 && hour < 17) {
+          timeOfDay = 'afternoon';
+        } else {
+          timeOfDay = 'evening';
+        }
+      
+        return { day, timeOfDay };
+    }
+      
+    // Usage example
+    const { day, timeOfDay } = getCurrentDayAndTimeOfDay();
+
     return ( 
         <div className="container main home" onClick={HideSideNav}>
             <div className="row">
-                <div className="col">
-
+                <div className="col day-greetings">
+                    <p>Good { timeOfDay }, have a happy { day }!</p>
+                </div>
+                <div className="col image-col">
+                    <div className="img-container">
+                        <img src={avatar} alt="avatar" />
+                    </div>
+                </div>
+                <div className="col name-col">
+                    <div className="name">
+                        <p>Marjohn</p>
+                        <p>Tomatao</p>
+                    </div>
                 </div>
             </div>
         </div>
