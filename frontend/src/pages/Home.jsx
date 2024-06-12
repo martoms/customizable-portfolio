4021b60226f8b7952408177d942c058b390c26cd
martomz <tomataomarjohn@gmail.com>
2024-06-12 20:48:21 +0800

    add dynamic changing background illustration

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import hideSideNav from "../script_dependencies/hideSideNav";
-import avatar from '../images/martoms-transparent.svg'
+import avatar from '../images/martoms-transparent.svg';
 import dayAndTime from "../script_dependencies/dayAndTime";
 import name from '../images/name-logo.svg';
 import Typed from 'typed.js';
 import HomeNav from "../components/home/HomeNav";
+import dev from '../images/dev-bg.svg';
+import designer from '../images/designer-bg.svg';
 
 const Home = () => {
     
@@ -13,6 +15,8 @@ const Home = () => {
 
     // for title
     const el = useRef(null);
+    const [currentText, setCurrentText] = useState('');
+
     useEffect(() => {
         const typed = new Typed(el.current, {
           strings: ['Programmer', 'Graphic Designer'],
@@ -22,15 +26,36 @@ const Home = () => {
           backSpeed: 100,
           loop: true,
           loopCount: Infinity,
+          onStringTyped: (arrayPos, self) => {
+              setCurrentText(self.strings[arrayPos]);
+          },
         });
     
         return () => {
           typed.destroy();
         };
-      }, []);
+    }, []);
+
+    const currentBG = () => {
+        const illustration = document.querySelector('#illustration');
+        if (currentText === 'Programmer') {
+            illustration.style.mixBlendMode = 'color-dodge'
+            return dev;
+        }
+
+        if (currentText === 'Graphic Designer') {
+            illustration.style.mixBlendMode = 'overlay'
+            return designer;
+        }
+
+        return dev;
+    }
 
     return ( 
         <div className="container main home" onClick={hideSideNav}>
+            <div className="bg">
+                <img id="illustration" src={currentBG()} alt="background illustration" />
+            </div>
             <div className="row">
                 <div className="col day-greetings">
                     <p>Good { timeOfDay },</p>
@@ -51,7 +76,7 @@ const Home = () => {
             </div>
             <div className="row">
                 <div className="col title-col">
-                    <h5>Hi, I am a <span ref={el} /></h5>
+                    <h5>Hi, I am a <span className="title" ref={el} /></h5>
                 </div>
             </div>
             <div className="selection"></div>
